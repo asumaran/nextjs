f398b67299e8248ece6d94b25d8a93df4843baf2
Alfredo Sumaran <a.sumaran@gmail.com>
2020-09-15 22:39:14 -0500

    More tweaks

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #444;
+  font-family: sans-serif;
+`;
+
+export default function Home() {
+  return <Title>Hello world!</Title>;
+}
